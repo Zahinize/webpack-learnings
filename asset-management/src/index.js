1e1d42cf0bde8c78926ee4b81fa2696932e3b117
Zahin Omar Alwa <zahin.alwa@gmail.com>
2019-10-23 02:59:47 +0530

    [Code changes] (Frontend: Webpack) Asset Management: Added tutorial example

---
+++ asset-management/src/index.js
@@ -0,0 +1,19 @@
+import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+
+function component() {
+	const element = document.createElement('div');
+
+	element.innerHTML = _.join(['Hello', 'Webpack'], '');
+	element.classList.add('hello');
+
+	const myIcon = new Image();
+
+	myIcon.src = Icon;
+	element.appendChild(myIcon);
+
+	return element;
+}
+
+document.body.appendChild(component());
